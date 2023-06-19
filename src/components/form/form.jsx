d89306fff64d4455bda318310528cbe4ae3100a9
Edgar Tribaldo <edgarjtribaldo@gmail.com>
2023-06-18 23:46:35 -0500

    v(1)

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -5,7 +5,7 @@ const Form = () => {
   return <section className="formulario"> <form >
     <h2>Crea tu colaborador</h2>
     <InputText titulo="Cual es el nombre de tu Colaborador?" placeholder="Nombre Colaborador"/>
-    <InputText titulo="Area a ubicar" placeholder="Desarrollo..."/>
+    <InputText titulo="Area a ubicar" placeholder="Desarrollo"/>
     <InputText titulo="Area a ubicar" placeholder="Escribe el neo"/>
 
   </form>
--- src/components/inputText/index.jsx
+++ src/components/inputText/index.jsx
@@ -1,10 +1,11 @@
 import './inputText.css'
 
 const InputText = (label) => {
+  const placemoficado = `${label.placeholder}...`;
   console.log(label.titulo + " hola");
   return <div className='campo-texto'>
     <label>{label.titulo}</label>
-    <input type="text" placeholder={label.placeholder}/>
+    <input type="text" placeholder={placemoficado}/>
   </div>
 }
 
