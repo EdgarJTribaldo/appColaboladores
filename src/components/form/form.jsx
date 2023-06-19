cf4ece7c5ae7339b075c674fda6dfdcbd858c0c1
Edgar Tribaldo <edgarjtribaldo@gmail.com>
2023-06-18 20:58:16 -0500

    begin

---
+++ src/components/form/form.jsx
@@ -0,0 +1,13 @@
+import './form.css'
+import InputText from '../inputText'
+
+const Form = () => {
+  return <form className="formulario">
+    <h2>Crea tu colaborador</h2>
+    <InputText/>
+    <InputText/>
+
+  </form>
+}
+
+export default Form;
---
+++ src/components/inputText/index.jsx
@@ -0,0 +1,10 @@
+import './inputText.css'
+
+const InputText = () => {
+  return <div className='campo-texto'>
+    <label htmlFor="">Colaborador</label>
+    <input type="text" placeholder='Nombre del colaborar'/>
+  </div>
+}
+
+export default InputText;
